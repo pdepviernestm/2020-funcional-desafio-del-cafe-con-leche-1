79201153bbbb530fdbe8ed3322c3ff2973ac7df7
Juan Manuel Fernandes dos Santos <juan.fernandes@10pines.com>
2020-05-08 00:46:32 -0300

    Initial commit

---
+++ src/Library.hs
@@ -0,0 +1,11 @@
+module Library where
+import PdePreludat
+import Data.Char (toUpper)
+
+obtenerElemento = implementame
+
+positivos = implementame
+
+estaOrdenada = implementame
+
+enMayusculas = implementame
